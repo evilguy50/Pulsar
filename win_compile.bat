5286a9e24420cf56f6547928693ee0a82b8fa355
evilguy50 <robbryan42@gmail.com>
2022-05-09 15:59:05 -0700

    Update win_compile.bat

--- win_compile.bat
+++ win_compile.bat
@@ -1,7 +1,7 @@
 IF EXIST ".\pulsar.exe" (del .\pulsar.exe)
 IF EXIST ".\pulsar_min.exe" (del .\pulsar_min.exe)
 IF EXIST ".\pulsar_cli.exe" (del .\pulsar_cli.exe)
-nimble install --depsOnly
+nimble install --depsOnly -y
 nim c -d:release --gc:orc -d:useMalloc pulsar_cli.nim
 nim --app:gui c -d:release --gc:orc -d:useMalloc .\pulsar.nim
 nim --app:gui c -d:release --gc:orc -d:useMalloc .\pulsar_min.nim
